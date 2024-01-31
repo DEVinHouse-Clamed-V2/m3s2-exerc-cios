d516208215505fe2ee70a890ff6d3eec802bf52e
lcsreiser <lcssreiser@gmail.com>
2024-01-30 21:33:42 -0300

    feat: aula terça feira

--- src/App copy.jsx
+++ src/App copy.jsx
@@ -1,36 +1,23 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react';
 import './App.css'
 
-function App() {
-  const [todos, setTodos] = useState(["Fazer comida"])
-  const [todo, setTodo] = useState("")
+export default function App() {
+  const [data, setData] = useState([]);
+  const [number, setNumber] = useState(1);
 
-  const handleAddTodo = (t) => {
-    setTodos([
-      ...todos,
-      t
-    ]);
-  }
+  useEffect(() =>{
+    fetch(`https://api.thecatapi.com/v1/images/search?limit=${number}`).then(response => response.json()).then(jsonResponse => setData(jsonResponse))
+  }, [number]);
 
-  const deleteTodo = (t) =>{
-    setTodos(todos.filter(e => e !== t))
-  }
-
-  return(
+  return (
     <>
-    <input type="text" value={todo} onChange={(e) => setTodo(e.target.value)}/>
-    <button onClick={() => {
-      handleAddTodo(todo)
-      setTodo("")
-    }}>add</button>
-    {todos.map((e,i) => (
-      <div className='div-container' key={i}>
-        <p>{e}</p>
-        <button onClick={() => deleteTodo(e)}>delete</button>
-      </div>
-    ))}
-    </>
-  )
-}
+      {data.map((e,i) =>(
+        <div key={i}>
+          <img src={e.url} alt="image" width={250} height={250}/>
+        </div>
+      ))}
 
-export default App
+      <button onClick={() => setNumber(10)}>+</button>
+    </>
+  );
+}
--- src/App.jsx
+++ src/App.jsx
@@ -1,43 +1,23 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react';
 import './App.css'
 
-function createInitialTodos() {
-  const initialTodos = [];
-  for (let i = 1; i <= 50; i++) {
-    initialTodos.push({
-      id: i,
-      text: 'Item ' + (i)
-    });
-  }
-  return initialTodos;
-}
+export default function App() {
+  const [data, setData] = useState([]);
+  const [number, setNumber] = useState(1);
 
-function App() {
-  const [todos, setTodos] = useState(createInitialTodos);
-  const [text, setText] = useState('');
+  useEffect(() =>{
+    fetch(`https://api.thecatapi.com/v1/images/search?limit=${number}`).then(response => response.json()).then(jsonResponse => setData(jsonResponse))
+  }, [number]);
 
   return (
     <>
-      <input
-        value={text}
-        onChange={e => setText(e.target.value)}
-      />
-      <button onClick={() => {
-        setText('');
-        setTodos([{
-          id: todos.length,
-          text: text
-        }, ...todos]);
-      }}>Add</button>
-      <ul>
-        {todos.map(item => (
-          <li key={item.id}>
-            {item.text}
-          </li>
-        ))}
-      </ul>
+      {data.map((e,i) =>(
+        <div key={i}>
+          <img src={e.url} alt="image" width={250} height={250}/>
+        </div>
+      ))}
+
+      <button onClick={() => setNumber(10)}>+</button>
     </>
   );
 }
-
-export default App
