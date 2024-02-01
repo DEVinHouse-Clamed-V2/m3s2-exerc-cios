d0e9b14736c83efa7379d133cc6d6bf846a99e14
lcsreiser <lcssreiser@gmail.com>
2024-02-01 18:59:15 -0300

    feat: aula3

--- src/main.jsx
+++ src/main.jsx
@@ -2,25 +2,12 @@ import * as React from "react";
 import * as ReactDOM from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "./index.css";
-import Root from "./routes/root";
-import ErrorPage from "./error-page";
-import Contact from "./routes/contact";
+import MainPage from "./pages";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <Root />,
-    errorElement: <ErrorPage />,
-    children: [
-      {
-        path: "contacts/:contactId",
-        element: <Contact />,
-      },
-    ],
-  },
-  {
-    path: "/v1",
-    element: <div>Ola mundo</div>,
+    element: <MainPage />,
   },
 ]);
 
